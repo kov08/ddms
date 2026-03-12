a2baf45583a9b782e9c2084c9c0372219b39fc4e
kov08 <krushn01@gmail.com>
2026-03-12 13:03:27 -0400

    Refactor(TransactionManager): Add  EVENT LOG tags and add demon thread to monitor the lock timestamp

--- src/main/java/com/d2db/transaction/TransactionManager.java
+++ src/main/java/com/d2db/transaction/TransactionManager.java
@@ -1,6 +1,5 @@
 package com.d2db.transaction;
 
-import java.security.PublicKey;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,13 +9,33 @@ import com.d2db.storage.CustomFileWriter;
 
 public class TransactionManager {
     private static TransactionManager instance;
-
     private boolean isTrasactionActive;
     private Map<String, Table> inMemoryWorkspace;
+    private long transactionStartTime;
+    private static final long TIMEOUT_MS = 60000; //60 seconds
 
     private TransactionManager() {
         isTrasactionActive = true;
         inMemoryWorkspace = new HashMap<>();
+        startWatchDogDemon();
+    }
+
+    private void startWatchDogDemon() {
+        Thread watchDog = new Thread(()->{
+            while (true) {
+                try {
+                    Thread.sleep(5000);
+                    if (isTrasactionActive && (System.currentTimeMillis() - transactionStartTime > TIMEOUT_MS)) {
+                        System.out.println("TIMEOUT: Forcefully rolling back abandoned transaction.");
+                        rollbackTransaction();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        watchDog.setDaemon(true);
+        watchDog.start();
     }
 
     public static synchronized TransactionManager getInstance() {
@@ -34,7 +53,9 @@ public class TransactionManager {
 
         this.isTrasactionActive = true;
         this.inMemoryWorkspace.clear();
-        System.out.println("TRANSACTION STARTED.");
+        this.transactionStartTime = System.currentTimeMillis();
+        // Pre-logging Step
+        System.out.println("[EVENT LOG] <StartTransaction> ID: " + transactionStartTime);
     }
 
     public Table getTableContext(String dbName, String tableName) throws Exception {
@@ -57,6 +78,7 @@ public class TransactionManager {
             throw new Exception("No active transaction to commit.");
         }
 
+        System.out.println("[EVENT LOG] <StartCommit> ID: " + transactionStartTime);
         CustomFileWriter writer = new CustomFileWriter(dbName);
 
         // Write/update all tables to storage from inmemory
@@ -64,6 +86,7 @@ public class TransactionManager {
             writer.writeTable(table);
         }
 
+        System.out.println("[EVENT LOG] <EndCommit> ID: " + transactionStartTime);
         this.isTrasactionActive = false;
         this.inMemoryWorkspace.clear();
         System.out.println("TRANSACTION COMMITED SUCCESSFULLY.");
@@ -75,6 +98,7 @@ public class TransactionManager {
             throw new Exception("No active transaction to rollback.");
         }
 
+        System.out.println("[EVENT LOG] <Rollback> ID: " + transactionStartTime);
         this.inMemoryWorkspace.clear();
         this.isTrasactionActive = false;
         System.out.println("TRANSACTION ROLLED BACK.");
